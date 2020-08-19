b7deed3e9d45fe78c53d3920ef17af7ba2ece524
jmath1 <jonathanamath@gmail.com>
2020-08-19 14:54:19 +0700

    change wsgi file to use env variable to indicate whether or not we are using containerization

--- wsgi.py
+++ wsgi.py
@@ -6,7 +6,11 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+
+if os.getenv("DOCKERBUILD"):
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "docker_settings")
+else:
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
