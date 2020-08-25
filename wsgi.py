77c7b4ad9a58f72f966c7f981a037a4510626df2
Jonathan Math <yonimath81@gmail.com>
2020-08-25 21:37:04 +0700

    remove alternative settings import from wsgi.py.

--- wsgi.py
+++ wsgi.py
@@ -7,10 +7,7 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 
 import os
 
-if os.getenv("DOCKERBUILD"):
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "docker_settings")
-else:
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
